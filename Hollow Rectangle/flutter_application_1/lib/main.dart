8acd17a072a98723b16a12fc5550c3ace68a92aa
ghasemizade <hosein@ghasemizade.com>
2022-07-02 10:19:12 -0700

    Hollow square

---
+++ Hollow Rectangle/flutter_application_1/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Draw Green Square'),
+        backgroundColor: Color(0xFF444444),
+      ),
+      body: ListView(children: <Widget>[
+        Text(
+          'Hosein Ghasemizade',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20, height: 2),
+        ),
+        Container(
+          width: 400,
+          height: 400,
+          child: CustomPaint(
+            painter: OpenPainter(),
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class OpenPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = Color(0xff638965)
+      ..style = PaintingStyle.fill;
+      StrokeCap.square;
+    //a rectangle
+    canvas.drawRect(Offset(100, 100) & Size(100, 100), paint1);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
